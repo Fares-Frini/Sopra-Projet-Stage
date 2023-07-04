6c24f118c1da8bf6d986548b13b915d491f53373
Fares-Frini <frini.mohamedfares@esprit.tn>
2023-07-04 15:43:00 +0100

    branch commit test

---
+++ src/main/java/com/stagiaire/springboot/model/User.java
@@ -0,0 +1,103 @@
+	package com.stagiaire.springboot.model;
+
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.stagiaire.springboot.encrypt.Encryptor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="mt_User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="emp_id")
+	private Long id;
+	
+	@Column(name="fname")
+	private String fname;
+	
+	@Column(name="lname")
+	private String lname;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="dob")
+	private Date dob;
+	
+	@Column(name="gender")
+	private String gender;
+	
+	@Column(name="role")
+	private String role;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) throws NoSuchAlgorithmException {
+		Encryptor encryptor = new Encryptor();
+		this.password = String.valueOf(encryptor.encryptString(password));
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(String dob)throws ParseException {
+		SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
+		this.dob=formatter.parse(dob);
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "User [fname=" + fname + ", lname=" + lname + ", email=" + email + ", password=" + password + ", dob="
+				+ dob + ", gender=" + gender + "]";
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
